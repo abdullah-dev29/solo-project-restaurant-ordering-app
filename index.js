52775e292959a04567f4a6878acbd751cfec8ac8
Abdullah <abdullah.01.29.2005@gmail.com>
2026-02-22 16:00:52 +0500

    Added Card Form

--- index.js
+++ index.js
@@ -18,9 +18,27 @@ function getFeedHtml() {
     return feedHtml
 }
 
+function getCardHtml () {
+    return `
+        <div class="card-details">
+            <div class="card-head">
+                <h3>Enter card details</h3>
+                <i class="fa-solid fa-xmark close-btn" data-close="close-card"></i>
+            </div>
+            <form action="" method="">
+                <input type="text" name="full-name" id="full-name" placeholder="Enter your name" required>
+                <input type="text" name="card-number" id="card-number" placeholder="Enter card number" required>
+                <input type="text" name="CVV" id="CVV" placeholder="Enter CVV">
+                <button class="payment">Pay</button>
+                </form>
+            </div>
+    `
+}
+
 function render() {
     const feedContainer = document.getElementById('feed')
     feedContainer.innerHTML = getFeedHtml()
+    document.querySelector('main').innerHTML += getCardHtml()
 
 }
 
