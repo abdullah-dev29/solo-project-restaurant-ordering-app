49cdbea25291dd1c9748aa4cbb8abc903b02dfa6
Abdullah <abdullah.01.29.2005@gmail.com>
2026-02-23 18:17:21 +0500

    Added Some more menu items

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const menuArray = [
+const menuArray =[
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
@@ -14,25 +14,53 @@ const menuArray = [
         id: 1
     },
     {
+        name: "Sushi",
+        ingredients: ["rice, nori, salmon, avocado, cucumber"],
+        price: 15,
+        emoji: "🍣",
+        id: 2
+    },
+    {
+        name: "Pasta",
+        ingredients: ["durum wheat, tomato sauce, garlic, basil"],
+        price: 13,
+        emoji: "🍝",
+        id: 3
+    },
+    {
+        name: "Taco",
+        ingredients: ["tortilla, beef, cheese, lettuce, salsa"],
+        price: 9,
+        emoji: "🌮",
+        id: 4
+    },
+    {
+        name: "Hot Dog",
+        ingredients: ["sausage, bun, mustard, ketchup"],
+        price: 8,
+        emoji: "🌭",
+        id: 5
+    },
+    {
+        name: "Ice Cream",
+        ingredients: ["milk, sugar, cream, vanilla"],
+        price: 6,
+        emoji: "🍨",
+        id: 6
+    },
+    {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 7
     },
     {
-    name: "Sushi",
-    ingredients: ["rice, nori, salmon, avocado, cucumber"],
-    price: 15,
-    emoji: "🍣",
-    id: 3
-    },
-    {
-    name: "Ice Cream",
-    ingredients: ["milk, sugar, cream, vanilla"],
-    price: 6,
-    emoji: "🍨",
-    id: 4
-}
+        name: "Coffee",
+        ingredients: ["coffee beans, water, milk, sugar"],
+        price: 5,
+        emoji: "☕",
+        id: 8
+    }
 ]
 export default menuArray
